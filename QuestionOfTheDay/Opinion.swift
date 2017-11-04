54f0e4016af8aeb392dddfc78f65964470d9ba99
Guest User <Guest@karthiks-air.nwmissouri.edu>
2017-11-04 18:46:16 -0500

    Created class opinion

---
+++ QuestionOfTheDay/Opinion.swift
@@ -0,0 +1,15 @@
+//
+//  Opinion.swift
+//  QuestionOfTheDay
+//
+//  Created by Guest User on 11/4/17.
+//
+
+import Foundation
+
+class Opinion {
+    var question:QuestionOfTheDay
+    var answer:Int
+    var objectId:String
+    
+}
---
+++ Statistician.swift
@@ -0,0 +1,24 @@
+//
+//  Statistician.swift
+//  
+//
+//  Created by Guest User on 11/4/17.
+//
+
+import Foundation
+
+class Statistician {
+    
+    func findPercentage() -> [Double]{
+        return [0.0]
+    }
+    func fetchQuestionOfTheDay() -> String{
+        return "Qyestion"
+    }
+    func saveOpinion(){
+        
+    }
+    init(){
+        
+    }
+}
