59416a38d59b97d7b1f84c92f547eae57fe38c29
Guest User <Guest@karthiks-air.nwmissouri.edu>
2017-11-04 18:27:54 -0500

    Created class QuestionOfTheDay

---
+++ QuestionOfTheDay/QuestionOfTheDay.swift
@@ -0,0 +1,31 @@
+//
+//  QuestionOfTheDay.swift
+//  QuestionOfTheDay
+//
+//  Created by Guest User on 11/4/17.
+//
+
+import Foundation
+
+class QuestionOfTheDay {
+    var question:String?
+    var answer0:String?
+    var answer1:String?
+    var answer2:String?
+    var objectId:String?
+    var create:NSDate?
+    var update:NSDate?
+    
+    init(question:String?, answer0:String?, answer1:String?, answer2:String?) {
+        self.question = question
+        self.answer0 = answer0
+        self.answer1 = answer1
+        self.answer2 = answer2
+    }
+    init(){
+        question = "What is my name?"
+        answer0 = "Karthik"
+        answer1 = "Mahesh Babu"
+        answer2 = "Leonardo Decaprio"
+    }
+}
