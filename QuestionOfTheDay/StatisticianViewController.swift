9012de83ec0f5315375884fd452607ecf59ca3cb
Guest User <Guest@karthiks-air.nwmissouri.edu>
2017-11-04 19:44:22 -0500

    Created view controller classes for vote and statistics tab and linked them accordingly

---
+++ QuestionOfTheDay/StatisticianViewController.swift
@@ -0,0 +1,34 @@
+//
+//  StatisticianViewController.swift
+//  QuestionOfTheDay
+//
+//  Created by Guest User on 11/4/17.
+//
+
+import UIKit
+
+class StatisticianViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
